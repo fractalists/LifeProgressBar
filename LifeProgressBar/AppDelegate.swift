2bf3cc7fc0d89ca397d005f22fe12841a3ebb5f5
JacobChengZhang <francislovem@gmail.com>
2018-06-14 09:54:22 +0800

    Initial Commit

---
+++ LifeProgressBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LifeProgressBar
+//
+//  Created by Apple on 2018/6/14.
+//  Copyright © 2018 xor. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
